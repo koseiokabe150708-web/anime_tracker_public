526ae35dd48b9a461863098f7a1ef639f161b4c4
Kosei Okabe <kosei.okabe150708@gmail.com>
2026-05-29 12:05:34 +0700

    Update anime table schema

--- migration/ddl_v1.sql
+++ migration/ddl_v1.sql
@@ -1,5 +1,7 @@
 BEGIN;
 
+DROP TABLE IF EXISTS anime;
+
 CREATE OR REPLACE FUNCTION fn_set_updated_at()
 RETURNS TRIGGER AS $$
 BEGIN
@@ -9,24 +11,21 @@ END;
 $$ LANGUAGE plpgsql;
 
 CREATE TABLE anime (
-
-  anime_id SERIAL PRIMARY KEY,
-
-  content_type TEXT NOT NULL
-    CHECK (content_type IN ('ANIME_SHOW', 'MOVIE')),
-
-  title TEXT NOT NULL,
-
-  watch_status TEXT,
-
-  rating INTEGER,
-
-  notes TEXT,
-
-  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-
+  anime_id        SERIAL PRIMARY KEY,
+  content_type    TEXT NOT NULL CHECK (content_type IN ('ANIME_SHOW', 'MOVIE')),
+  title           TEXT NOT NULL,
+  story_number    INTEGER,
+  positive_score  INTEGER CHECK (positive_score >= 0),
+  negative_score  INTEGER CHECK (negative_score >= 0),
+  opponent        TEXT,
+  rokuyo          TEXT,
+  notes           TEXT,
+  anime_date      DATE,
+  is_active       BOOLEAN NOT NULL DEFAULT TRUE,
+  character_appear       BOOLEAN NOT NULL DEFAULT TRUE,
+  watch_status    TEXT,
+  created_at      TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at      TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TRIGGER trg_anime_updated_at
